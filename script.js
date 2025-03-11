01a5a1ca4d55a5549e801e0d34befb0c83144b9b
Nandhakumar <nnandha545@gmail.com>
2025-03-11 14:55:49 +0530

    L

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ async function fetchWeather() {
               }]
             },
             options: {
-              responsive: true, 
+              responsive: false, 
               maintainAspectRatio: false,
               scales: {
                   x: {
